4d8ac052bd1feb2931f25fb2b0e002b09efe14a3
azjamsg <84362314+azjamsg@users.noreply.github.com>
2022-03-20 11:05:14 +0800

    Create ListsBasic.py

---
+++ ListsBasic.py
@@ -0,0 +1,16 @@
+friends = ['John','Michael','Terry','Eric','Graham']
+print(friends)
+
+#print the second friend's name
+print(friends[1])
+
+print(friends[1],friends[4])
+print(friends[2:4])
+
+#find number of elements in list
+print(len(friends))
+
+print(friends.index('Eric'))
+
+#count instances of element in list
+print(friends.count('Eric'))
