66edc30435e6f4dfdaad76db0e403ff47235f072
azjamsg <84362314+azjamsg@users.noreply.github.com>
2022-03-20 10:29:31 +0800

    Create DatatypesAndTypecasting.py

---
+++ DatatypesAndTypecasting.py
@@ -0,0 +1,20 @@
+# integer type
+failed_subjects=2
+# float type 
+failed_subjects=2.45  
+name='John'
+# boolean type
+is_going_to_party = True
+
+# string type
+print(type('hello'))
+print(type(1))
+print(type(1.64))
+print(type(True))
+
+#exercise
+item_name = 'widget'
+price = 23.5
+inventory = 100
+is_in_inventory = True
+print(item_name, price, inventory)
