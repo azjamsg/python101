80bb893beba988e844a8cefa5f662841b7e04245
azjamsg <84362314+azjamsg@users.noreply.github.com>
2022-03-20 17:51:53 +0800

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -16,3 +16,14 @@ def greeting(name,age=28):
 name = input("Enter your name: ")    
 greeting(name,32)
 greeting("Judith")
+
+#named functions
+def greeting(name, age=28, color="red"):
+ #Greets user with “name” from “input box” and “age”, if unavailable, default age is used   
+   
+   print(f"Hello {name.capitalize()}, you will be {age+1} next birthday!")
+   print(f"We hear you like the color {color.lower()}!")
+
+greeting(age=27, name="brian",color="Blue")
+
+Profile(yob=1995,weight=83.5,height=192,eye_color="blue")
