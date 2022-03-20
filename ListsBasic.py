f4efa0811aeff8cd81c9eb2f0e4605b152c9b2e5
azjamsg <84362314+azjamsg@users.noreply.github.com>
2022-03-20 11:18:59 +0800

    Update ListsBasic.py

--- ListsBasic.py
+++ ListsBasic.py
@@ -14,3 +14,36 @@ print(friends.index('Eric'))
 
 #count instances of element in list
 print(friends.count('Eric'))
+
+friends = ['John','Michael','Terry','Eric','Graham']
+cars = [911,130,328,535,740,308]
+print(friends)
+friends.sort()
+print(friends)
+# sort in reverse order
+friends.sort(reverse=True)
+print(friends)
+# print in reverse 
+friends.reverse()
+print(friends)
+
+print(min(cars))
+print(max(cars))
+
+friends.append('TerryG')
+
+#insert in index 1
+friends.insert(1,'TerryG')
+
+#extend list
+friends.extend(cars)
+
+friends.remove('Terry')
+
+#pop last element
+friends.pop()
+
+#copy list
+new_friends = friends[:] #only copy memory position
+new_friends = friends.copy
+new_friends = list(friends)
