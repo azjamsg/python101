2619ccf5599c85fc2c72dc6b4255c7a07e127ebb
azjamsg <84362314+azjamsg@users.noreply.github.com>
2022-03-20 11:31:02 +0800

    Create SplitAndJoin.py

---
+++ SplitAndJoin.py
@@ -0,0 +1,8 @@
+msg ='Welcome  to  Python  101: Split  and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(csv.split(','))
+print('-'.join(friends_list))
+
+#replace all spaces and join
+print(msg.replace(' ', ''))
